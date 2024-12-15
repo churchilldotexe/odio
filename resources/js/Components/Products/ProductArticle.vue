5cc2e0fac76e0e667f1a0e8b7c637b89ef0a908e
churchilldotexe <churchillexe@gmail.com>
2024-12-15 11:57:22 +0800

    chore: move product article to own component

---
+++ resources/js/Components/Products/ProductArticle.vue
@@ -0,0 +1,33 @@
+<template>
+    <article :class='["px-6 grid gap-10 max-w-screen-xl ", "md:gap-16 md:px-10 lg:grid-cols-2 xl:px-0 lg:gap-30 "]'>
+        <img class="rounded-lg w-full md:hidden" src="/assets/shared/mobile/image-best-gear.jpg"
+            alt="a guy with a headset">
+        <img class="hidden rounded-lg size-full md:block lg:hidden" src="/assets/shared/tablet/image-best-gear.jpg"
+            alt="a guy with a headset">
+
+        <img class="hidden rounded-lg size-full lg:block lg:order-2 " src="/assets/shared/desktop/image-best-gear.jpg"
+            alt="a guy with a headset">
+
+        <div class="space-y-8 md:px-14 lg:place-self-center ">
+            <h3
+                class="uppercase font-bold text-2.5-xl text-center text-balance mx-auto md:text-4xl md:text-pretty lg:order-1 lg:text-left lg:max-w-none lg:text-balance ">
+                Bringing
+                you
+                the <strong class=" text-coral">best</strong> audio
+                gear</h3>
+
+            <p class="font-medium text-black/50 md:text-center lg:text-left ">Located at the heart of New York
+                City,
+                Audiophile is
+                the
+                premier store for
+                high end headphones,
+                earphones, speakers, and audio accessories. We have a large showroom and luxury
+                demonstration
+                rooms available for you to browse and experience a wide range of our products. Stop by our
+                store
+                to meet some of the fantastic people who make Audiophile the best place to buy your portable
+                audio equipment.</p>
+        </div>
+    </article>
+</template>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -2,6 +2,7 @@
 import MainLayout from '@/Layouts/MainLayout.vue';
 import ProductCategoryGrid from '@/Components/Products/ProductCategoryGrid.vue';
 import ButtonLink from '@/Components/ButtonLink.vue';
+import ProductArticle from '@/Components/Products/ProductArticle.vue';
 
 </script>
 
@@ -90,10 +91,13 @@ import ButtonLink from '@/Components/ButtonLink.vue';
                 <section class="grid items-center  ">
                     <img class=" [grid-area:1/1] rounded-lg w-full md:hidden "
                         src="/assets/home/mobile/image-speaker-zx7.jpg">
+
                     <img class="hidden md:block [grid-area:1/1] rounded-lg w-full lg:hidden"
                         src="/assets/home/tablet/image-speaker-zx7.jpg">
-                    <img class="hidden md:block [grid-area:1/1] rounded-lg w-full"
+
+                    <img class="hidden lg:block [grid-area:1/1] rounded-lg w-full"
                         src="/assets/home/desktop/image-speaker-zx7.jpg">
+
                     <div class="[grid-area:1/1] justify-self-start pl-6 space-y-8 md:pl-16">
                         <h3 class="uppercase text-2.5-xl font-bold">zx7 speaker</h3>
                         <ButtonLink href="#"
@@ -119,38 +123,7 @@ import ButtonLink from '@/Components/ButtonLink.vue';
                 </section>
             </section>
 
-            <article
-                :class='["px-6 grid gap-10 max-w-screen-xl ", "md:gap-16 md:px-10 lg:grid-cols-2 xl:px-0 lg:gap-30 lg:py-20 "]'>
-                <img class="rounded-lg w-full md:hidden" src="/assets/shared/mobile/image-best-gear.jpg"
-                    alt="a guy with a headset">
-                <img class="hidden rounded-lg size-full md:block lg:hidden"
-                    src="/assets/shared/tablet/image-best-gear.jpg" alt="a guy with a headset">
-
-                <img class="hidden rounded-lg size-full lg:block lg:order-2 "
-                    src="/assets/shared/desktop/image-best-gear.jpg" alt="a guy with a headset">
-
-                <div class="space-y-8 md:px-14 lg:place-self-center ">
-                    <h3
-                        class="uppercase font-bold text-2.5-xl text-center text-balance mx-auto md:text-4xl md:text-pretty lg:order-1 lg:text-left lg:max-w-none lg:text-balance ">
-                        Bringing
-                        you
-                        the <strong class=" text-coral">best</strong> audio
-                        gear</h3>
-
-                    <p class="font-medium text-black/50 md:text-center lg:text-left ">Located at the heart of New York
-                        City,
-                        Audiophile is
-                        the
-                        premier store for
-                        high end headphones,
-                        earphones, speakers, and audio accessories. We have a large showroom and luxury
-                        demonstration
-                        rooms available for you to browse and experience a wide range of our products. Stop by our
-                        store
-                        to meet some of the fantastic people who make Audiophile the best place to buy your portable
-                        audio equipment.</p>
-                </div>
-            </article>
+            <ProductArticle class="lg:py-20 " />
         </div>
     </MainLayout>
 </template>
