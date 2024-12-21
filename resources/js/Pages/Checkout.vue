39ce256f8caa54791d3c944ca1a784b9ec66dd03
churchilldotexe <churchillexe@gmail.com>
2024-12-22 03:22:12 +0800

    feat: added checkout page

---
+++ resources/js/Pages/Checkout.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import MainLayout from '@/Layouts/MainLayout.vue';
+
+
+</script>
+
+<template>
+    <MainLayout title="Checkout Page">
+        <div>domo</div>
+    </MainLayout>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,10 @@ Route::get('/foo', function () {
     return Inertia::render('Details');
 });
 
+Route::get('/checkout', function () {
+    return Inertia::render('Checkout');
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
