cc6aeae9a47838b47881646b5da2ab3f98be491b
churchilldotexe <churchillexe@gmail.com>
2024-12-23 00:27:20 +0800

    style: order confirmation modal tablet and desktop view

--- resources/js/Components/Order/OrderConfirmation.vue
+++ resources/js/Components/Order/OrderConfirmation.vue
@@ -29,27 +29,32 @@ watch(() => showModal, (modal) => {
         leave-from-class="opacity-100" leave-to-class="opacity-0">
         <dialog class="rounded-lg  backdrop:bg-black/40 " ref="dialogRef" v-show="showModal"
             @click.self="handleCloseModal" @close="handleCloseModal">
-            <section class="grid gap-6 p-6">
+            <section class=" grid w-full max-w-screen-sm gap-6 p-6 md:gap-8 md:p-12">
                 <CheckIcon />
                 <div class="space-y-4">
-                    <h3 class="text-balance text-2xl font-bold uppercase">thank you for your order</h3>
+                    <h3 class=" text-2xl font-bold uppercase md:text-3xl">
+                        thank you
+                        <br />
+                        for your order
+                    </h3>
                     <p class="font-medium text-black/50">You will receive an email confirmation shortly.</p>
                 </div>
-                <div class="">
-                    <div class="grid gap-6 rounded-t-lg bg-[#f1f1f1] p-6 ">
-                        <OrderOverview class="place-self-center">
+                <div class="grid md:grid-cols-[1fr,1fr]">
+                    <div class="grid gap-6 rounded-t-lg bg-[#f1f1f1] p-6 md:w-max ">
+                        <OrderOverview class="place-self-center ">
                             <p class=" self-start justify-self-end pt-2 font-bold text-black/50">x1</p>
                         </OrderOverview>
                         <hr class="border border-black/[0.08]" />
                         <p class="text-center text-xs font-bold text-black/50">and 2 other item(s)</p>
                     </div>
-                    <div class="grid gap-2 rounded-b-lg bg-black p-6 ">
-                        <p class="font-medium uppercase text-white/50">grand total</p>
-                        <p class="text-lg font-bold text-white">$ 5,446</p>
+                    <div
+                        class="grid gap-2 rounded-b-lg bg-black p-6 md:w-full md:rounded-r-lg md:rounded-bl-none md:py-10">
+                        <p class="font-medium uppercase text-white/50 md:self-end">grand total</p>
+                        <p class="text-lg font-bold text-white ">$ 5,446</p>
                     </div>
 
                 </div>
-                <ButtonLink class="h-fit w-full text-center" href="/">back to home</ButtonLink>
+                <ButtonLink class="mt-3 h-fit w-full text-center" href="/">back to home</ButtonLink>
             </section>
         </dialog>
 
