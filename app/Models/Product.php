f9dd74a343711eda233af72eaf543fe70973dff9
churchilldotexe <churchillexe@gmail.com>
2024-12-29 04:14:02 +0800

    chore: created casts method with enums for validation

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
@@ -18,4 +19,41 @@ class Product extends Model
         'description',
         'features',
     ];
+
+    /**
+     * @return HasMany<ProductImage,Product>
+     */
+    public function productImages(): HasMany
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+    /**
+     * @return HasMany<GalleryImage,Product>
+     */
+    public function galleryImages(): HasMany
+    {
+        return $this->hasMany(GalleryImage::class);
+    }
+
+    /**
+     * @return HasMany<ProductInclusion,Product>
+     */
+    public function productInclusions(): HasMany
+    {
+        return $this->hasMany(ProductInclusion::class);
+    }
+
+    public function createWithImages()
+    {
+        // assumming that the product is itterated
+        // create the product and get the return
+        // the return id will be used to create the other table
+
+        // $this->productImages()->create($productImages);
+        // $this->productImages()->create($galleryImages);
+        // $this->productImages()->create($productInclusions);
+
+        return $this->load('productImages', 'galleryImages', 'productInclusions');
+    }
 }
--- app/Models/ProductInclusion.php
+++ app/Models/ProductInclusion.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProductInclusion extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductInclusionFactory> */
     use HasFactory;
+
+    protected $fillable = ['product_id', 'item_name', 'quantity'];
+
+    /**
+     * @return BelongsTo<Product,ProductImage>
+     */
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
