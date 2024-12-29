740b94bef8ab7c4905a31ff0fb27ff5f3d853ab5
churchilldotexe <churchillexe@gmail.com>
2024-12-30 01:57:25 +0800

    test: common case for the seeding the data to database

--- tests/Feature/Models/ProductTest.php
+++ tests/Feature/Models/ProductTest.php
@@ -1,35 +1,61 @@
 <?php
 
+use App\Models\GalleryImage;
 use App\Models\Product;
+use App\Models\ProductImage;
+use App\Models\ProductInclusion;
 use Database\Seeders\ProductSeeder;
 
-it('should have products', function () {
-    $products = Product::factory()->create();
+describe('Product Model Database queries', function () {
+    it('should have products', function () {
+        $products = Product::factory()->create();
 
-    expect(Product::where('id', $products->id)->exists())->toBeTrue();
+        expect(Product::where('id', $products->id)->exists())->toBeTrue();
 
-    expect($products)->toMatchArray([
-        'id' => $products->id,
-        'name' => $products->name,
-    ]);
+        expect($products)->toMatchArray([
+            'id' => $products->id,
+            'name' => $products->name,
+        ]);
+    });
+
+    it('should populate db using seed', function () {
+        $this->seed(ProductSeeder::class);
+
+        $seededProduct = Product::count();
+        expect($seededProduct)->toBeGreaterThan(5);
+
+        $exampleProduct = Product::where('name', 'YX1 Wireless Earphones')->first();
+        expect($exampleProduct)->not->toBeNull();
+        expect($exampleProduct->category)->toBe('earphones');
+    });
 });
 
-it('should populate db using seed', function () {
-    $this->seed(ProductSeeder::class);
+describe('Product model helper createWithAllRelatedTables', function () {
 
-    $seededProduct = Product::count();
-    expect($seededProduct)->toBeGreaterThan(5);
+    it('should also create and seed the productImages table through helper', function () {
 
-    $exampleProduct = Product::where('name', 'YX1 Wireless Earphones')->first();
-    expect($exampleProduct)->not->toBeNull();
-    expect($exampleProduct->category)->toBe('earphones');
-});
+        $this->seed(ProductSeeder::class);
 
-it('should also create the related tables ', function () {
-    $product = Product::factory()->create();
+        expect(Product::count())->toEqual(6);
+        expect(ProductImage::count())->toEqual(36);
 
-    $products = $product->createWithImages();
+    });
 
-    expect(Product::where('id', $product->id)->exists())->toBeTrue();
+    it('should also create and seed the galleryImages table through helper', function () {
 
+        $this->seed(ProductSeeder::class);
+
+        expect(Product::count())->toEqual(6);
+        expect(GalleryImage::count())->toEqual(54);
+
+    });
+
+    it('should also create and seed the ProductInclusion table through helper', function () {
+
+        $this->seed(ProductSeeder::class);
+
+        expect(Product::count())->toEqual(6);
+        expect(ProductInclusion::count())->toEqual(28);
+
+    });
 });
