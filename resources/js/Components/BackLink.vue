83b6c288a512e2b483dcd80d00630af878bbb889
churchilldotexe <churchillexe@gmail.com>
2025-01-02 23:40:05 +0800

    chore: change link to use history back api

--- resources/js/Components/BackLink.vue
+++ resources/js/Components/BackLink.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 import { Link } from '@inertiajs/vue3';
+
+
+const goBack = () => {
+    history.back()
+}
 </script>
 
 <template>
 
     <!-- TODO: go back to its parent (e.g. /earphone or history stack) -->
     <div class="mx-auto max-w-screen-xl px-6 pt-6 font-medium capitalize text-black/50 md:pt-8 lg:pt-20 xl:px-0">
-        <Link class="hocus-visible:text-coral " href="/">
+        <Link class="hocus-visible:text-coral " href="/" @click.prevent="goBack">
         go back
         </Link>
     </div>
