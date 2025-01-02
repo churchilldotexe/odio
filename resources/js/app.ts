27ab3507f61d98ae247942136ca04eb13110dda2
churchilldotexe <churchillexe@gmail.com>
2025-01-02 23:35:12 +0800

    feat: add pinia state management
    to handle cart states

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -3,6 +3,7 @@ import { createInertiaApp } from '@inertiajs/vue3';
 
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
+import { createPinia } from 'pinia';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import '../css/app.css';
 import './bootstrap';
@@ -10,19 +11,20 @@ import './bootstrap';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-  title: title => `${title} - ${appName}`,
-  resolve: name =>
-    resolvePageComponent(
-      `./Pages/${name}.vue`,
-      import.meta.glob<DefineComponent>('./Pages/**/*.vue')
-    ),
-  setup({ el, App, props, plugin }) {
-    createApp({ render: () => h(App, props) })
-      .use(plugin)
-      .use(ZiggyVue)
-      .mount(el);
-  },
-  progress: {
-    color: '#4B5563'
-  }
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob<DefineComponent>('./Pages/**/*.vue')
+        ),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .use(createPinia())
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
 });
