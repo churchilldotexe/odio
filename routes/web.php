d4f0d962f7c8465ab0d1c9f357b0999e5b2288f1
churchilldotexe <churchillexe@gmail.com>
2025-01-04 16:55:07 +0800

    refactor: remove unused routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Enums\ProductCategory;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
@@ -26,23 +25,6 @@ Route::controller(ProductController::class)->group(function () {
     Route::get('/products/{category}', 'index')->name('products');
     Route::get('/products/{category}/{product}', 'show')->name('products.show');
 
-});
-
-Route::get('/headphone', function () {
-    return Inertia::render('HeadPhone');
-});
-
-Route::get('/speaker', function () {
-    return Inertia::render('Speaker');
-});
-
-Route::get('/earphone', function () {
-    return Inertia::render('EarPhone');
-});
-
-// placeholder for show
-Route::get('/foo', function () {
-    return Inertia::render('Details');
 });
 
 Route::get('/checkout', function () {
