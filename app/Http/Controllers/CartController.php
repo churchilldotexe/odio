1aee8ad9bea94024d47c518143824352cfc16cbc
churchilldotexe <churchillexe@gmail.com>
2025-01-06 16:09:40 +0800

    chore: init cart controller for session cart management

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        dd($request);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Request $request)
+    {
+        dd($request);
+    }
+}
